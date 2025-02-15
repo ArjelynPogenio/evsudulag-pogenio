9114fda8def73142b205a2348c4e33ee3323a2d3
arjielyn <arjielyn.pogenio@gmail.com>
2025-02-15 16:47:28 +0800

    adding the form

--- login/src/app/module/users.module.ts
+++ login/src/app/module/users.module.ts
@@ -2,14 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { RoutesModule } from './routes.module';
-
+import { MaterialModule } from './material.module';
+import { ReactiveFormsModule } from '@angular/forms'
 
 
 @NgModule({
   declarations: [ LoginComponent ],
   imports: [
     CommonModule,
-    RoutesModule
+    RoutesModule,
+    MaterialModule,
+    ReactiveFormsModule
   ]
 })
 export class UsersModule { }
