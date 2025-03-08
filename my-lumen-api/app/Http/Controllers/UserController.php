37b86c4154fb1b8b21acd6865c156301104a3058
juliana <arjielyn>
2025-03-08 15:14:34 +0800

    lumen post api

---
+++ my-lumen-api/app/Http/Controllers/UserController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    public function login(Request $request)
+    {
+
+        $this->validate($request, [
+            'username' => 'required|string',
+            'password' => 'required|string'
+        ]);
+
+        $user = DB::table('users')->where('username', $request->username)->first();
+
+        if (!$user || $request->password !== $user->password) {
+            return response()->json([
+                'message' => 'Invalid username or password'
+            ], 401);
+        }
+
+        return response()->json([
+            'message' => 'Login successful',
+            'user' => [
+                'id' => $user->id,
+                'username' => $user->username,
+                'name' => $user->name,
+                'age' => $user->age,
+                'birthday' => $user->birthday
+            ]
+        ]);
+    }
+}
